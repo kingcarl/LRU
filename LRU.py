a3506c4389aae3502c19c938d4d868f7093ad1cb
KingCarl <kingcarl725@163.com>
2014-10-08 15:24:42 +0800

    修改已知BUG

--- LRU.py
+++ LRU.py
@@ -1,74 +1,124 @@
-#@Author: Carl
-#@Date: 2014/3/21
-#@description: LRU Algorithm
+#-*- coding:UTF-8 -*-
+#=============================================================================
+#     FileName: LRU.py
+#         Desc: LRU Algorithm
+#       Author: guannipishi
+#        Email: kingcarl725@163.com
+#      Version: 0.1
+#   LastChange: 2014-03-21 15:44:25
+#      History:
+#=============================================================================
 
-LRU_NODE_LEN = 10
+import time
+
+LRU_NODE_NUM_DEFAULT = 10
 LRU_QUEUE_LEN_DEFUALT = 10
 
-class LRU_Node(object):
-	"""LRU Node infomation"""
-	def __init__(self, item=None, num=0):
-		self.item = item
-		self.num = num
+class LruNode:
+    """LRU Node infomation"""
+    def __init__(self, item=None, num=1):
+        self.item = item
+        self.num = num
+        self.ltime = time.time()
 
-	def update(self):
-		self.num += 1
+    def update(self):
+        self.ltime = time.time()
+        self.num += 1
 
-class LRU_Entry(object):
-	"""LRU Node List"""
-	def __init__(self, max_item_num):
-		self.max_item_num = max_item_num
-		self.lru_nodes = []
+class LruEntry:
+    """LRU Node List"""
+    def __init__(self, max_item_num, lock):
+        self.max_item_num = max_item_num
+        self.lru_nodes = []
+        if lock:
+            from threading import Lock
+            self.lru_lock = Lock()
+        else:
+            from multiprocessing import Lock
+            self.lru_lock = Lock()
 
-	def insertNodeInEntry(self, item):
-		if len(self.lru_nodes) <= self.max_item_num:
-			pos = self.findNodeInEntry(item)
-			if (pos < 0):
-				self.lru_nodes.append(LRU_Node(item=item))
-			else:
-				self.lru_nodes[pos].update()
+    def _insert_node_in_entry(self, item, full=False):
+        pos = self.find_node_in_entry(item)
+        self.lru_lock.acquire()
+        if pos < 0:
+            if not full:
+                self.lru_nodes.append(LruNode(item=item))
+            else:
+                self.lru_nodes.pop(0)
+                self.lru_nodes.append(LruNode(item=item))
+        else:
+            self.lru_nodes[pos].update()
+        self.lru_lock.release()
 
-			""" sort Lru Node List to make sure that the minimum node on the top of list"""	
-			self.lru_nodes = sorted(self.lru_nodes, key=lambda x: x.num )
-		else:
-			self.deleteNodeInEntry()
+    def insert_node_in_entry(self, item):
+        if len(self.lru_nodes) <= self.max_item_num:
+            self._insert_node_in_entry(item)
 
-	def deleteNodeInEntry(self):
-		self.lru_nodes.pop(0)
-		
-	def findNodeInEntry(self, item):
-		pos = 0
-		for li in self.lru_nodes:
-			if item == li.item:
-				return (pos)
-			else:
-				pos += 1
-		return (-1)
+            """ 
+            [1]sort Lru Node List to make sure that the minimum item at the top of list
+            [2]based on step 1, sort Lru Node List to make sure nearest item at the bottom of list
+            """
+            self.lru_lock.acquire()
+            self.lru_nodes = sorted(self.lru_nodes, key=lambda x: x.num)
+            self.lru_nodes = sorted(self.lru_nodes, key=lambda x: x.ltime)
+            self.lru_lock.release()
+        else:
+            self._insert_node_in_entry(item, True)
+            self.delete_node_in_entry()
 
-class LRU_Queue(object):
-	def __init__(self, maxlen):
-		self.max_bucket_len = maxlen
-		self.LRU_Queue_Bucket = [ n for n in range(self.max_bucket_len) ] #initialize bucket
+    def delete_node_in_entry(self):
+        self.lru_lock.acquire()
+        self.lru_nodes.pop(0)
+        self.lru_lock.release()
 
-	def insertNode(self, item):
-		key = self.lruHash(item)
-		if self.findNode(key):
-			self.LRU_Queue_Bucket[key].insertNodeInEntry(item)
-		else:
-			self.LRU_Queue_Bucket[key] = LRU_Entry(LRU_NODE_LEN)
-			self.LRU_Queue_Bucket[key].insertNodeInEntry(item)
-	
-	def findNode(self, key):
-		if isinstance(self.LRU_Queue_Bucket[key], LRU_Entry):
-			return True	
-		else:
-			return False
+    def find_node_in_entry(self, item):
+        pos = 0
+        self.lru_lock.acquire()
+        for li in self.lru_nodes:
+            if item == li.item:
+                self.lru_lock.release()
+                return pos
+            else:
+                pos += 1
+        self.lru_lock.release()
 
-	def lruHash(self, item):
-		return (hash(item) / self.max_bucket_len) % self.max_bucket_len
-		
-	def showALL(self):
-		for le in self.LRU_Queue_Bucket:
-			if isinstance(le, LRU_Entry):
-				for ln in le.lru_nodes:
-					print "%s  %d" % (ln.item, ln.num)	
+        return -1
+
+class LruQueue:
+    """
+    `max_queue_len`: the maximum length of lru queue
+    `max_item_num`: the maximum number of lru queue entry
+    `tlock`: True (thread lock), False (process lock), default value is False
+    """
+    def __init__(self, max_queue_len=LRU_QUEUE_LEN_DEFUALT, max_item_num=LRU_NODE_NUM_DEFAULT, tlock=False):
+        self.max_queue_len = max_queue_len
+        self.max_item_num = max_item_num
+        self.tlock = tlock
+        self.LRU_QUEUE_BUCKET = [ n for n in range(self.max_queue_len) ] 
+
+    def insert_node(self, item):
+        key = self.lru_hash(item)
+        if self.find_node(key):
+            self.LRU_QUEUE_BUCKET[key].insert_node_in_entry(item)
+        else:
+            self.LRU_QUEUE_BUCKET[key] = LruEntry(self.max_item_num, self.tlock)
+            self.LRU_QUEUE_BUCKET[key].insert_node_in_entry(item)
+
+    def find_node(self, item):
+        key = self.lru_hash(item)
+        if isinstance(self.LRU_QUEUE_BUCKET[key], LruEntry):
+            return True
+        else:
+            return False
+
+    def lru_hash(self, item):
+        return (hash(item) / self.max_queue_len) % self.max_queue_len
+
+    def get_data(self):
+        return self.LRU_QUEUE_BUCKET
+
+    def print_queue_info(self):
+        for le in self.LRU_QUEUE_BUCKET:
+            if isinstance(le, LruEntry):
+                for ln in le.lru_nodes:
+                    print "item[{0}], num[{1}]".format(ln.item, ln.num)
